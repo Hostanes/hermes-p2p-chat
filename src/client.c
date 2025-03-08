1c9df0aa7bab0093dee6723c86abce4d631e012f
PazPacifica <Rueslin.Rana@outlook.com>
2025-03-08 15:01:20 +0200

    added connection functioanlity, issue with both threads readin from same stdin at the same time

--- src/client.c
+++ src/client.c
@@ -10,18 +10,18 @@
 #include <sys/socket.h>
 #include <unistd.h>
 
-// TODO move below to header file
-
-#define PORT 8081
 #define MAX_CLIENTS 10
 #define READ_BUFFER_SIZE 1024
 #define WRITE_BUFFER_SIZE 1024
 
-// message types definitions
-
+// Message types definitions
 #define LEAVE 'l'
 #define JOIN 'j'
 #define TEXT 't'
+#define CONNECT_REQUEST 'c'
+#define CONNECT_ACCEPT 'a'
+#define CONNECT_REJECT 'r'
+#define CONNECT_CONFIRM 'f'
 
 typedef struct {
   int user_id; // local variable
@@ -47,25 +47,16 @@ typedef struct {
 typedef struct {
   char type;
   char content[256];
-
 } packet_core_t;
-
-// temp structs
-typedef struct {
-
-} receive_parameter;
-
-typedef struct {
-
-} write_parameter;
 
 room_t *rooms[4];
 int room_count = 0;
 
-user_t *users[5];
+user_t *users[MAX_CLIENTS];
 int user_count = 0;
 
 int server_fd;
+int current_port; // Current user's port
 
 user_t *create_User(struct sockaddr_in addr, int sockfd) {
   user_t *user = (user_t *)malloc(sizeof(user_t));
@@ -81,17 +72,99 @@ user_t *create_User(struct sockaddr_in addr, int sockfd) {
 }
 
 void *accept_connections(void *arg) {
-  //
+  struct sockaddr_in address;
+  int addrlen = sizeof(address);
+  int new_socket;
+
+  address.sin_family = AF_INET;
+  address.sin_addr.s_addr = INADDR_ANY;
+  address.sin_port = htons(current_port); // Use the current port
+
+  if (bind(server_fd, (struct sockaddr *)&address, sizeof(address)) < 0) {
+    perror("Bind failed");
+    exit(EXIT_FAILURE);
+  }
+
+  struct sockaddr_in sin;
+  socklen_t len = sizeof(sin);
+  if (getsockname(server_fd, (struct sockaddr *)&sin, &len) == -1) {
+    perror("getsockname failed");
+  } else {
+    current_port = ntohs(sin.sin_port);
+    printf("Server is listening on port %d\n", current_port);
+  }
+
+  if (listen(server_fd, 3) < 0) {
+    perror("Listen failed");
+    exit(EXIT_FAILURE);
+  }
+
+  printf("Listening for incoming connections...\n");
+
+  while (1) {
+    if ((new_socket = accept(server_fd, (struct sockaddr *)&address,
+                             (socklen_t *)&addrlen)) < 0) {
+      perror("Accept failed");
+      continue;
+    }
+    printf("New connection accepted from %s:%d\n", inet_ntoa(address.sin_addr),
+           ntohs(address.sin_port));
+
+    packet_core_t packet;
+    if (recv(new_socket, &packet, sizeof(packet), 0) < 0) {
+      perror("Receive failed");
+      close(new_socket);
+      continue;
+    }
+
+    if (packet.type == CONNECT_REQUEST) {
+      printf("Connection request received from %s\n", packet.content);
+
+      printf("Do you want to accept the connection? (y/n): ");
+      char response;
+      scanf(" %c", &response);
+
+      packet_core_t response_packet;
+      if (response == 'y') {
+        response_packet.type = CONNECT_ACCEPT;
+        strcpy(response_packet.content, "Connection accepted");
+      } else {
+        response_packet.type = CONNECT_REJECT;
+        strcpy(response_packet.content, "Connection rejected");
+      }
+
+      // Send response
+      if (send(new_socket, &response_packet, sizeof(response_packet), 0) < 0) {
+        perror("Send failed");
+        close(new_socket);
+        continue;
+      }
+
+      if (response == 'y') {
+        // Wait for final confirmation
+        if (recv(new_socket, &packet, sizeof(packet), 0) < 0) {
+          perror("Receive failed");
+          close(new_socket);
+          continue;
+        }
+
+        if (packet.type == CONNECT_CONFIRM) {
+          // Add user to list
+          users[user_count] = create_User(address, new_socket);
+          user_count++;
+          printf("User added to list\n");
+        }
+      }
+    }
+
+    close(new_socket);
+  }
 
   return NULL;
 }
 
-void *receive_Thread() {}
-
-int send_Text(char msg_type) { return 0; }
-
-int request_Connection(const char *peer_ip) {
-  printf("Request connection\n");
+int request_Connection(const char *peer_ip, int target_port) {
+  printf("Requesting connection to %s:%d\n", peer_ip, target_port);
 
   int sockfd;
   struct sockaddr_in server_addr;
@@ -104,7 +177,7 @@ int request_Connection(const char *peer_ip) {
   }
 
   server_addr.sin_family = AF_INET;
-  server_addr.sin_port = htons(PORT);
+  server_addr.sin_port = htons(target_port);
 
   if (inet_pton(AF_INET, peer_ip, &server_addr.sin_addr) <= 0) {
     printf("Invalid server addr error\n");
@@ -117,27 +190,69 @@ int request_Connection(const char *peer_ip) {
     return -1;
   }
 
-  printf("Connected to peer at %s\n", peer_ip);
+  printf("Connected to peer at %s:%d\n", peer_ip, target_port);
 
-  user_t new_user;
-  users[user_count] = create_User(server_addr, sockfd);
-  user_count++;
+  // Send connection request
+  packet_core_t packet;
+  packet.type = CONNECT_REQUEST;
+  strcpy(packet.content, "Connection request");
 
+  if (send(sockfd, &packet, sizeof(packet), 0) < 0) {
+    perror("Send failed");
+    close(sockfd);
+    return -1;
+  }
+
+  // Receive response
+  if (recv(sockfd, &packet, sizeof(packet), 0) < 0) {
+    perror("Receive failed");
+    close(sockfd);
+    return -1;
+  }
+
+  if (packet.type == CONNECT_ACCEPT) {
+    printf("Connection accepted by peer\n");
+
+    // Send final confirmation
+    packet.type = CONNECT_CONFIRM;
+    strcpy(packet.content, "Connection confirmed");
+
+    if (send(sockfd, &packet, sizeof(packet), 0) < 0) {
+      perror("Send failed");
+      close(sockfd);
+      return -1;
+    }
+
+    // Add user to list
+    users[user_count] = create_User(server_addr, sockfd);
+    user_count++;
+    printf("User added to list\n");
+  } else if (packet.type == CONNECT_REJECT) {
+    printf("Connection rejected by peer\n");
+  }
+
+  close(sockfd);
   return 0;
 }
 
 int prep_Connection_Request() {
   char ip_address[16];
+  int target_port;
 
   printf("Request a connection:\n");
-  printf("Input IP addr \n ");
+  printf("Input IP address: ");
   if (fgets(ip_address, sizeof(ip_address), stdin) != NULL) {
-    ip_address[strcspn(ip_address, "\n")] = '\0';
+    ip_address[strcspn(ip_address, "\n")] = '\0'; // Remove newline
 
     if (strlen(ip_address) > 0) {
-      printf("Connecting to %s...\n", ip_address);
+      printf("Input target PORT number: ");
+      scanf("%d", &target_port);
+      getchar();
+
+      printf("Connecting to %s:%d...\n", ip_address, target_port);
+      request_Connection(ip_address, target_port);
     } else {
-      printf("Invalid IP addr entered.\n");
+      printf("Invalid IP address entered.\n");
     }
   } else {
     printf("Error reading input.\n");
@@ -147,11 +262,11 @@ int prep_Connection_Request() {
 }
 
 void *send_Thread() {
-
   char write_buffer[WRITE_BUFFER_SIZE] = {0};
 
-  while (1) {
+  printf("Input a message\n");
 
+  while (1) {
     char msg_type;
 
     scanf(" %c", &msg_type);
@@ -159,13 +274,11 @@ void *send_Thread() {
 
     switch (msg_type) {
     case LEAVE:
-
       break;
     case JOIN:
       prep_Connection_Request();
       break;
     case TEXT:
-
       break;
     }
     msg_type = 'x';
@@ -175,20 +288,25 @@ void *send_Thread() {
 }
 
 int main(int argc, char const *argv[]) {
+  pthread_t sender_th;
+  pthread_t receiver_th;
 
-  pthread_t *sender_th;
-  pthread_t *receiver_th;
+  do {
+    printf("Enter the PORT number to listen on (0 for dynamic allocation): ");
+    scanf("%d", &current_port);
+    getchar();
+  } while (current_port < 0 || current_port > 65535);
 
   if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0) {
     perror("socket create failed");
     return -1;
   }
 
-  pthread_create(sender_th, NULL, send_Thread, NULL);
-  pthread_create(receiver_th, NULL, receive_Thread, NULL);
+  pthread_create(&sender_th, NULL, send_Thread, NULL);
+  pthread_create(&receiver_th, NULL, accept_connections, NULL);
 
-  pthread_join(*sender_th, NULL);
-  pthread_join(*receiver_th, NULL);
+  pthread_join(sender_th, NULL);
+  pthread_join(receiver_th, NULL);
 
   close(server_fd);
 
